39868cb814119eac410cac0b66c74bbbf8add133
Hieu <buihieuduy@gmail.com>
2026-05-20 09:00:00 +0700

    chore(backend): add runtime dependencies and config

--- backend/src/main/java/com/jpassistant/JpAssistantApplication.java
+++ backend/src/main/java/com/jpassistant/JpAssistantApplication.java
@@ -1,12 +1,18 @@
 package com.jpassistant;
 
 import com.jpassistant.config.AiServiceProperties;
+import com.jpassistant.config.AuthProperties;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jReactiveRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jRepositoriesAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@EnableConfigurationProperties(AiServiceProperties.class)
+@SpringBootApplication(exclude = {
+        Neo4jRepositoriesAutoConfiguration.class,
+        Neo4jReactiveRepositoriesAutoConfiguration.class
+})
+@EnableConfigurationProperties({AiServiceProperties.class, AuthProperties.class})
 public class JpAssistantApplication {
 
     /**
